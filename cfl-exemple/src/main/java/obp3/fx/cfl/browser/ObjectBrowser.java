f2681392705d274f146436d97a60c7ebe38384f5
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2025-07-09 23:06:08 +0200

    a more detailed example

--- cfl-exemple/src/main/java/obp3/fx/cfl/browser/ObjectBrowser.java
+++ cfl-exemple/src/main/java/obp3/fx/cfl/browser/ObjectBrowser.java
@@ -4,8 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import obp3.cfl.model.Epsilon;
-import obp3.cfl.model.Product;
+import obp3.cfl.model.*;
 import obp3.fx.objectbrowser.ObjectViewContainer;
 import obp3.fx.objectbrowser.ObjectViewFactory;
 import obp3.fx.objectbrowser.api.ObjectView;
@@ -19,8 +18,29 @@ public class ObjectBrowser extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("Object Browser");
 
-        Object sample = new Product(new Product(new Epsilon(), new Epsilon()), new Epsilon());
+//        Object sample = new Product(new Product(new Epsilon(), new Epsilon()), new Epsilon());
 //        ObjectViewFactory.register(Person.class, PersonView.class);
+
+
+
+        var rS = new Reference('S');
+        var rA = new Reference('A');
+        var rB = new Reference('B');
+        var rC = new Reference('C');
+        var rD = new Reference('D');
+        var s  = new Product(rA, rB);
+        var a  = new Sum(rC, new Epsilon());
+        var b  = new Sum (rD, new Epsilon());
+        var c  = new Product(new Token<>('a'), new Product(rA, rD));
+        var d  = new Product(new Token<>('b'), new Product(rB, rC));
+        rS.target = s;
+        rA.target = a;
+        rB.target = b;
+        rC.target = c;
+        rD.target = d;
+        var sample = s;
+
+
         ObjectView view = ObjectViewFactory.instance.createView(sample);
         root.setCenter(new ObjectViewContainer( sample ).getView());
 //        root.setBottom(new ObjectViewContainer( new Point(2, 3)).getView());
